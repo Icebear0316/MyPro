223546484ddfe657ae25f7d916568c10ee73b1ef
DELL <Icebear08042@163.com>
2023-06-13 15:53:54 +0800

    测试使用Mybatis Plus的增删改

--- tea-admin-server/src/main/java/cn/tedu/tea/admin/server/content/pojo/entity/Tag.java
+++ tea-admin-server/src/main/java/cn/tedu/tea/admin/server/content/pojo/entity/Tag.java
@@ -1,5 +1,7 @@
 package cn.tedu.tea.admin.server.content.pojo.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
@@ -18,6 +20,7 @@ public class Tag implements Serializable {
     /**
      * 数据ID
      */
+    @TableId(type = IdType.AUTO)
     private Long id;
     /**
      * 标签名称
--- tea-admin-server/src/test/java/cn/tedu/tea/admin/server/content/dao/persiti/mapper/TagMapperTests.java
+++ tea-admin-server/src/test/java/cn/tedu/tea/admin/server/content/dao/persiti/mapper/TagMapperTests.java
@@ -3,9 +3,15 @@ package cn.tedu.tea.admin.server.content.dao.persiti.mapper;
 
 import cn.tedu.tea.admin.server.content.dao.persist.mapper.TagMapper;
 import cn.tedu.tea.admin.server.content.pojo.entity.Tag;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 public class TagMapperTests {
@@ -16,9 +22,70 @@ public class TagMapperTests {
     @Test
     void insert() {
         Tag tag = new Tag();
-        tag.setName("测试标签名称");
+        tag.setName("测试标签名称998");
 
-        mapper.insert(tag);
+        System.out.println("插入数据之前，参数：" + tag);
+        int rows = mapper.insert(tag);
+        System.out.println("插入数据成功，受影响的行数：" + rows);
+        System.out.println("插入数据之后，参数：" + tag);
+    }
+
+    @Test
+    void deleteById() {
+        Long id = 1L;
+        int rows = mapper.deleteById(id);
+        System.out.println("删除数据成功，受影响的行数：" + rows);
+    }
+
+    @Test
+    void deleteByMap() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", 1);
+        int rows = mapper.deleteByMap(map);
+        System.out.println("删除数据成功，受影响的行数：" + rows);
+    }
+
+    @Test
+    void delete() {
+        QueryWrapper<Tag> wrapper = new QueryWrapper<>();
+        wrapper.eq("id", 1);
+        int rows = mapper.delete(wrapper);
+        System.out.println("删除数据成功，受影响的行数：" + rows);
+    }
+    @Test
+    void deleteBatchIds() {
+        List<Long> idList = new ArrayList<>();
+        idList.add(4L);
+        idList.add(5L);
+        idList.add(6L);
+
+        int rows = mapper.deleteBatchIds(idList);
+        System.out.println("删除数据成功，受影响的行数：" + rows);
+    }
+
+    @Test
+    void updateById() {
+        Tag tag = new Tag();
+        tag.setId(1L);
+        // tag.setName("测试标签名称998");
+        tag.setEnable(1);
+        tag.setSort(198);
+
+        int rows = mapper.updateById(tag);
+        System.out.println("修改数据成功，受影响的行数：" + rows);
+    }
+
+    @Test
+    void update() {
+        Tag tag = new Tag();
+        tag.setEnable(1);
+        tag.setSort(197);
+
+        QueryWrapper<Tag> wrapper = new QueryWrapper<>();
+        wrapper.eq("id", 1);
+
+        int rows = mapper.update(tag, wrapper);
+        System.out.println("修改数据成功，受影响的行数：" + rows);
     }
 
 }
