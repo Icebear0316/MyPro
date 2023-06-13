2e77ea432fd1e324e482afeab111fac534d828a0
DELL <Icebear08042@163.com>
2023-06-13 10:33:49 +0800

    创建数据库然后在application.properties文件中将正确的数据库名字写好,在测试类中进行测试

--- tea-admin-server/src/test/java/cn/tedu/tea/admin/server/TeaAdminServerApplicationTests.java
+++ tea-admin-server/src/test/java/cn/tedu/tea/admin/server/TeaAdminServerApplicationTests.java
@@ -1,13 +1,25 @@
 package cn.tedu.tea.admin.server;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.DatabaseMetaData;
 
 @SpringBootTest
 class TeaAdminServerApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Autowired
+    DataSource dataSource;
+
+    @Test
+    void getConnection() throws Throwable {
+        dataSource.getConnection();
     }
 
 }
