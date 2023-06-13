bc4064469c6001277ed8e27af3a0e4c5b9a1ee1a
DELL <Icebear08042@163.com>
2023-06-13 09:11:41 +0800

    茶叶项目

---
+++ tea-admin-server/src/main/java/cn/tedu/tea/admin/server/TeaAdminServerApplication.java
@@ -0,0 +1,13 @@
+package cn.tedu.tea.admin.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeaAdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeaAdminServerApplication.class, args);
+    }
+
+}
---
+++ tea-admin-server/src/test/java/cn/tedu/tea/admin/server/TeaAdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tedu.tea.admin.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeaAdminServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
