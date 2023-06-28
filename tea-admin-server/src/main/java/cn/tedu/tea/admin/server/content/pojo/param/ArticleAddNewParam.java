677fde0c886b9b37cb86a12fb46375265bf40ad0
DELL <Icebear08042@163.com>
2023-06-28 14:54:37 +0800

    param

--- tea-admin-server/src/main/java/cn/tedu/tea/admin/server/content/pojo/param/ArticleAddNewParam.java
+++ tea-admin-server/src/main/java/cn/tedu/tea/admin/server/content/pojo/param/ArticleAddNewParam.java
@@ -1,5 +1,6 @@
 package cn.tedu.tea.admin.server.content.pojo.param;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -13,10 +14,6 @@ import java.io.Serializable;
 @Data
 public class ArticleAddNewParam implements Serializable {
 
-    /**
-     * 数据ID
-     */
-    private Long id;
     /**
      * 作者ID
      */
@@ -40,11 +37,8 @@ public class ArticleAddNewParam implements Serializable {
     /**
      * 标签列表，实际存入JSON数据
      */
+    @ApiModelProperty(value = "标签列表，实际存入JSON数据", example = "[{\"id\":10, \"name\":\"Spring\"},{\"id\":11, \"name\":\"Spring MVC\"}]")
     private String tags;
-    /**
-     * IP地址
-     */
-    private String ip;
     /**
      * 排序序号
      */
@@ -54,32 +48,8 @@ public class ArticleAddNewParam implements Serializable {
      */
     private String coverUrl;
     /**
-     * 顶数量
+     * 详情
      */
-    private Integer upCount;
-    /**
-     * 踩数量
-     */
-    private Integer downCount;
-    /**
-     * 浏览量
-     */
-    private Integer clickCount;
-    /**
-     * 评论量
-     */
-    private Integer commentCount;
-    /**
-     * 审核状态，0=未审核，1=审核通过，2=拒绝审核
-     */
-    private Integer checkState;
-    /**
-     *
-     */
-    private String checkRemarks;
-    /**
-     * 显示状态，0=不显示，1=显示
-     */
-    private Integer displayState;
+    private String detail;
 
 }
